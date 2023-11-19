28afc4820b36508bfd2fffb3f5580a015a416ba7
AMD <michojekunle1@gmail.com>
2023-11-19 08:22:09 +0100

    made changes

--- auth.ts
+++ auth.ts
@@ -1,4 +1,5 @@
 import NextAuth from "next-auth";
 import Github from 'next-auth/providers/Github';
 import Google from 'next-auth/providers/google';
+
 export const { handlers, auth } = NextAuth({ providers: [Github, Google] })
