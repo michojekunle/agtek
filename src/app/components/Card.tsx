a6e5389c94c6de3eb7263fd836a97ee99e000463
AMD <michojekunle1@gmail.com>
2023-11-22 12:55:27 +0100

    made changes

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -9,20 +9,21 @@ type Props = {
 
 function Card({ name, testimonial, avatar }: Props) {
   return (
-    <div className=''>
-        <div className=''>
+    <div className='bg-white rounded-md shadow-lg border pt-32 p-11 relative my-11 w-full max-w-xl'>
+        <div className='absolute -top-7 left-2/4 -translate-x-2/4'>
             <Image
                 src={avatar}
                 alt="avatar"
-                className='rounded-full w-44 h-44'
+                width={500}
+                height={500}
+                className='rounded-full w-16 h-16'
             />
         </div>
         <h4 className='text-dark_blue font-extrabold ms:text-lg text-xl'>{name}</h4>
         <p className='leading-relaxed md:text-md text-sm text-dark_grayish_blue'>{testimonial}</p>
-        <div>
+        <div className=''>
 
         </div>
-
     </div>
   )
 }
--- src/app/components/TestimonialsCarousel.tsx
+++ src/app/components/TestimonialsCarousel.tsx
@@ -6,13 +6,15 @@ import Card from './Card';
 type Props = {}
 
 function myArrow({ type, onClick, isEdge }) {
-    return
+    return <span></span>
   }
 
 function TestimonialsCarousel({}: Props) {
   return (
-    <div className='w-full:'>
-    <Carousel itemsToShow={1} renderArrow={myArrow}>
+    <div className='w-full py-24'>
+    <Carousel itemsToShow={1} renderArrow={myArrow} className='py-11'>
+      <Card avatar='/assets/images/avatar-anisha.png' testimonial="Manage has supercharged our team's workflow" name='Anisha Li'/>
+      <Card avatar='/assets/images/avatar-anisha.png' testimonial="Manage has supercharged our team's workflow" name='Anisha Li'/>
       <Card avatar='/assets/images/avatar-anisha.png' testimonial="Manage has supercharged our team's workflow" name='Anisha Li'/>
     </Carousel>
   </div>
